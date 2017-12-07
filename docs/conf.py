f37ba4316aad7a9e5a2b7c9483b6d8bf9c4a3861
yanlind <yanlind@andrew.cmu.edu>
2017-12-07 16:14:57 -0500

    added setup and requirement file

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax', 'numpydoc', 'sphinx.ext.autosummary']
+    'sphinx.ext.mathjax', 'sphinxcontrib.napoleon', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odyssey",
+    version="0.1",
+    packages=find_packages(exclude=['joblib', 'docs', 'tests', '.cache']),
+
+    author="Alan (Yanlin) Duan",
+    author_email="duanyanl97@gmail.com",
+    description="Tools for analyzing python package usage on GitHub through Google BigQuery",
+    license="MIT",
+    keywords="package usage analysis BigQuery GitHub",
+    url="https://github.com/alan97/odyssey",
+)
