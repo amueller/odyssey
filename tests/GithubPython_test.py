7931683dbbffacae44071cc174d417cb9e10aa67
Andreas Mueller <andreas.mueller@columbia.edu>
2018-11-13 17:31:40 -0500

    autopep8 after fixing syntax error

--- tests/GithubPython_test.py
+++ tests/GithubPython_test.py
@@ -4,23 +4,23 @@ from odyssey.core.bigquery.GithubPython import GithubPython
 
 class TestGithubPython(unittest.TestCase):
 
-	def test_instantiation(self):
-		a = GithubPython()
-		self.assertTrue(a.package == "")
-		self.assertEqual(a.exclude_forks, "auto")
-		b = GithubPython("sklearn", False)
-		self.assertTrue(b.package == "sklearn")
-		self.assertFalse(b.exclude_forks == True)
+    def test_instantiation(self):
+        a = GithubPython()
+        self.assertTrue(a.package == "")
+        self.assertEqual(a.exclude_forks, "auto")
+        b = GithubPython("sklearn", False)
+        self.assertTrue(b.package == "sklearn")
+        self.assertFalse(b.exclude_forks == True)
 
-	def test_build_exclude_repo_string(self):
-		a = GithubPython("sklearn")
-		self.assertEqual(a._contains_package_string(),
-			'REGEXP_CONTAINS(content, "sklearn")')
+    def test_build_exclude_repo_string(self):
+        a = GithubPython("sklearn")
+        self.assertEqual(a._contains_package_string(),
+                         'REGEXP_CONTAINS(content, "sklearn")')
 
-	def test_get_query(self):
-		a = GithubPython("sklearn", False)
-		self.assertTrue("content, repo_name, path"
-			in a._get_all_query())
-		self.assertTrue('REGEXP_CONTAINS(content,"sklearn"')
-			in a._get_all_query())
-		self.assertTrue("count(*)" in a._get_count_query())
+    def test_get_query(self):
+        a = GithubPython("sklearn", False)
+        self.assertTrue("content, repo_name, path"
+                        in a._get_all_query())
+        self.assertTrue('REGEXP_CONTAINS(content,"sklearn")'
+                        in a._get_all_query())
+        self.assertTrue("count(*)" in a._get_count_query())
